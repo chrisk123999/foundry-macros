b9d77a9ab34fea49ac7743f03a80dd0432363efa
Chris <chrisk123999@gmail.com>
2022-11-04 02:49:09 -0700

    Added Spike Growth Spell

---
+++ Spells/Spike Growth/Spike Growth.md
@@ -0,0 +1,12 @@
+# Module Requirements  
+Midi-Qol (and it's own associated requirements)  
+Template Macros  
+# Notes  
+- Currently the token won't take damage if it passes through the template but does not stop in it. This is a limitation of template macros.  
+- The scene must use "ft" as it's grid.  
+# Setup Instructions  
+- Copy and paste the macro from "TemplateWhenEnteredWhenLeft.js" into the "When Entered" and "When Left" field from template macros.  
+- Copy and paste the macro from "TemplateWhenStaying.js" into the "When Staying" field from template macros.
+- Configure the Spike Growth spell as pictured below:  
+![Spell Details 1](SpellDetails1.PNG)  
+![Spell Details 2](SpellDetails2.PNG)  
---
+++ Spells/Spike Growth/TemplateWhenEnteredWhenLeft.js
@@ -0,0 +1,48 @@
+if (canvas.scene.grid.units != 'ft') return;
+if (this.hook.animate === false) return;
+let cellDistance;
+if (canvas.scene.grid.type != 0) {
+	let through = this.hook.templatemacro.through.find(tmp => tmp.templateId === template.id);
+	if (!through) return;
+	cellDistance = through.cells.length;
+} else {
+	let token = canvas.tokens.get(this.tokenId);
+	let currentTokenCenter = {
+		x: this.coords.current.x + (token.w / 2),
+		y: this.coords.current.y + (token.w / 2)
+	};
+	let previousTokenCenter = {
+		x: this.coords.previous.x + (token.w / 2),
+		y: this.coords.previous.y + (token.w / 2)
+	};
+	let intersectionPoint = quadraticIntersection(previousTokenCenter, currentTokenCenter, template.center, template.shape.radius, epsilon=0);
+	if (intersectionPoint.length === 0) return;
+	let ray = new Ray(intersectionPoint[0], currentTokenCenter);
+	cellDistance = (Math.ceil(ray.distance / canvas.scene.grid.size));
+}
+let scale = Math.ceil(canvas.scene.grid.distance / 5);
+let distance = cellDistance * scale;
+if (distance <= 0) return;
+for (let i = 0; i < distance; i++) {
+	let damageDice = '2d4[piercing]';
+	let diceRoll = await new Roll(damageDice).roll({async: true});
+	let diceTotal = diceRoll.total;
+	diceRoll.toMessage({
+		rollMode: 'roll',
+		speaker: {alias: name},
+		flavor: 'Spike Growth Damage'
+	});
+	let token = canvas.tokens.get(this.tokenId);
+	await MidiQOL.applyTokenDamage(
+		[
+			{
+				damage: diceTotal,
+				type: 'piercing'
+			}
+		],
+		diceTotal,
+		new Set([token]),
+		null,
+		null
+	);
+}
