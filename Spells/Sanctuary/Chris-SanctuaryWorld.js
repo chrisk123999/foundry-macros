378638a3fc50507658effb81e60a4d00305a1166
Chris <chrisk123999@gmail.com>
2022-12-14 18:55:02 -0800

    Multiclassing fix

--- Spells/Sanctuary/Chris-SanctuaryWorld.js
+++ Spells/Sanctuary/Chris-SanctuaryWorld.js
@@ -4,8 +4,14 @@ Hooks.on('midi-qol.AttackRollComplete', async workflow => {
     let targetEffect = targetToken.actor.effects.find(eff => eff.label === 'Sanctuary');
     if (!targetEffect) return;
     let targetActor = targetToken.actor;
-    let spellDC = targetActor.flags.world?.spell?.sanctuary;
-    if (!spellDC) return;
+    let targetItem = await fromUuid(targetEffect.origin);
+    let scaling = targetItem.system.save.scaling;
+    let spellDC;
+    if (scaling === 'spell') {
+        spellDC = targetItem.parent.system.attributes.spelldc;
+    } else {
+        spellDC = targetItem.parent.system.abilities[scaling].dc;
+    }
     let sourceActor = workflow.actor;
     let save = await sourceActor.rollAbilitySave('wis');
     if (save.total >= spellDC) return;
