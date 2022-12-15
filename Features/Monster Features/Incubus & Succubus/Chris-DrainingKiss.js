c22874b13cbee1bebb51b84b80696255a07ea895
Chris <chrisk123999@gmail.com>
2022-12-15 01:47:01 -0800

    Update Chris-DrainingKiss.js

--- Features/Monster Features/Incubus & Succubus/Chris-DrainingKiss.js
+++ Features/Monster Features/Incubus & Succubus/Chris-DrainingKiss.js
@@ -1,12 +1,51 @@
-if (args[0].failedSaves.length != 1) return;
-let targetToken = args[0].failedSaves[0];
+function chris = {
+    'findEffect': function _findEffect(actor, name) {
+        return actor.effects.find(eff => eff.label === name);
+    },
+    'createEffect': async function _createEffect(actor, effectData) {
+        if (game.user.isGM) {
+            await actor.createEmbeddedDocuments('ActiveEffect', [effectData]);
+        } else {
+            await MidiQOL.socket().executeAsGM('createEffects', {'actorUuid': actor.uuid, 'effects': [effectData]});
+        }
+    }
+    'updateEffect': async function _updateEffect(effect, updates) {
+        if (game.user.isGM) {
+            await effect.update(updates);
+        } else {
+            updates._id = effect.id;
+            await MidiQOL.socket().executeAsGM('updateEffects', {'actorUuid': effect.parent.uuid, 'updates': [updates]});
+        }
+    },
+    'addCondition': async function _addCondition(actor, name, overlay, origin) {
+        await game.dfreds.effectInterface.addEffect(
+            {
+                'effectName': name,
+                'uuid': actor.uuid,
+                'origin': origin,
+                'overlay': overlay
+            }
+        );
+    },
+    'removeCondition': async function _removeCondition(actor, name) {
+        await game.dfreds.effectInterface.removeEffect(
+            {
+                'effectName': name,
+                'uuid': actor.uuid
+            }
+        );
+    }
+};
+if (args[0].targets.length != 1) return;
+let targetToken = args[0].targets[0];
 let targetActor = targetToken.actor;
-let targetEffect = targetActor.effects.find(eff => eff.label === 'Draining Kiss');
+let featureName = args[0].item.name;
+let targetEffect = chris.findEffect(targetActor, featureName);
 let appliedDamage = 0 - args[0].damageList[0].appliedDamage;
 if (!targetEffect) {
     let effectData = {
-		'label': 'Draining Kiss',
-		'icon': 'icons/magic/air/wind-vortex-swirl-purple.webp',
+		'label': featureName,
+		'icon': args[0].item.img,
 		'duration': {
 			'seconds': 604800
 		},
@@ -26,7 +65,7 @@ if (!targetEffect) {
 			}
 		}
 	};
-	await MidiQOL.socket().executeAsGM('createEffects', {'actorUuid': targetActor.uuid, 'effects': [effectData]});
+	await chris.createEffect(targetActor, effectData);
 } else {
     let oldAppliedDamage = ~~targetEffect.changes[0].value;
     appliedDamage += oldAppliedDamage;
@@ -41,22 +80,10 @@ if (!targetEffect) {
 			}
 		]
     };
-    await MidiQOL.socket().executeAsGM('updateEffects', {'actorUuid': targetActor.uuid, 'updates': [updates]});
+    await chris.updateEffect(targetEffect, updates);
 }
 let targetMaxHP = targetActor.system.attributes.hp.max;
 if (Math.abs(appliedDamage) >= targetMaxHP) {
-    await game.dfreds.effectInterface.removeEffect(
-        {
-            'effectName': 'Unconscious',
-            'uuid': targetActor.uuid
-        }
-    );
-    await game.dfreds.effectInterface.addEffect(
-        {
-            'effectName': 'Dead',
-            'uuid': targetActor.uuid,
-            'origin': args[0].item.uuid,
-            'overlay': true
-        }
-    );
+    await chris.removeCondition(targetActor, 'Unconscious');
+    await chris.addCondition(targetActor, 'Dead', true, null)
 }
