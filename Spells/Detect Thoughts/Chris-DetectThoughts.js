e8c4fdebbd57bda45031e0ded50dda8cefc563c6
Chris <chrisk123999@gmail.com>
2022-12-24 13:20:25 -0800

    Update Chris-DetectThoughts.js

--- Spells/Detect Thoughts/Chris-DetectThoughts.js
+++ Spells/Detect Thoughts/Chris-DetectThoughts.js
@@ -1,3 +1,20 @@
+let chris = {
+    'removeEffect': async function _removeEffect(effect) {
+        if (game.user.isGM) {
+            await effect.delete();
+        } else {
+            await MidiQOL.socket().executeAsGM('removeEffects', {'actorUuid': effect.parent.uuid, 'effects': [effect.id]});
+        }
+    },
+    'removeCondition': async function _removeCondition(actor, name) {
+        await game.dfreds.effectInterface.removeEffect(
+            {
+                'effectName': name,
+                'uuid': actor.uuid
+            }
+        );
+    }
+};
 let effect = chris.findEffect(this.actor, 'Detect Thoughts');
 if (!effect) return;
 await chris.removeEffect(effect);
