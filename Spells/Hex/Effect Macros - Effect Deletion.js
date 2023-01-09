fe0e83955929c23987c974c2f8fd5bbb90940c2a
Chris <chrisk123999@gmail.com>
2023-01-09 09:42:15 -0800

    Update Effect Macros - Effect Deletion.js

--- Spells/Hex/Effect Macros - Effect Deletion.js
+++ Spells/Hex/Effect Macros - Effect Deletion.js
@@ -1,3 +1,15 @@
+let chris = {
+    'findEffect': function _findEffect(actor, name) {
+        return actor.effects.find(eff => eff.label === name);
+    },
+    'removeEffect': async function _removeEffect(effect) {
+        if (game.user.isGM) {
+            await effect.delete();
+        } else {
+            await MidiQOL.socket().executeAsGM('removeEffects', {'actorUuid': effect.parent.uuid, 'effects': [effect.id]});
+        }
+    }
+};
 let targetTokenId = effect.changes[0].value;
 let targetToken = canvas.scene.tokens.get(targetTokenId);
 if (!targetToken) return;
