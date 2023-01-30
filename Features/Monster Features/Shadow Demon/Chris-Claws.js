964c19218b30451f0b999593ef228ad78164ef41
Chris <chrisk123999@gmail.com>
2023-01-29 22:52:23 -0800

    Create Chris-Claws.js

---
+++ Features/Monster Features/Shadow Demon/Chris-Claws.js
@@ -0,0 +1,8 @@
+if (this.hitTargets.size != 1) return;
+if (this.advantage != true) return;
+if (this.advantage && this.disadvantage) return;
+let damageDice = 4;
+if (this.isCritical) damageDice = 8;
+let damageFormula = damageDice + 'd6[psychic] + 3';
+let damageRoll = await new Roll(damageFormula).roll({async: true});
+await this.setDamageRoll(damageRoll);
