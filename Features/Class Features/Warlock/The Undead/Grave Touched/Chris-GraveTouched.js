882cf561c2c70d5359b1fc54cb84cca08905c5d1
Chris <chrisk123999@gmail.com>
2023-01-31 14:09:13 -0800

    Create Chris-GraveTouched.js

---
+++ Features/Class Features/Warlock/The Undead/Grave Touched/Chris-GraveTouched.js
@@ -0,0 +1,49 @@
+let chris = {
+    'dialog': async function _dialog(title, options) {
+        let buttons = options.map(([label,value]) => ({label,value}));
+        let selected = await warpgate.buttonDialog(
+            {
+                buttons,
+                title,
+            },
+            'column'
+        );
+        return selected;
+    }
+};
+if (this.hitTargets.size != 1) return;
+let validTypes = ['msak', 'rsak', 'mwak', 'rwak'];
+if (!validTypes.includes(this.item.system.actionType)) return;
+let feature = this.actor.items.getName('Grave Touched');
+if (!feature) return;
+let currentTurn = '';
+let doCheck = false;
+if (game.combat === null || game.combat === undefined) {
+    doCheck = true;
+} else {
+    if (this.token.id != game.combat.current.tokenId) return;
+    currentTurn = game.combat.round + '-' + game.combat.turn;
+    let previousTurn = feature.flags.world?.feature?.gt?.turn;
+    if (!previousTurn || previousTurn != currentTurn) doCheck = true;
+}
+if (!doCheck) return;
+let selection = await chris.dialog('Use Grave Touched?', [['Yes', true], ['No', false]]);
+if (!selection) return;
+await feature.setFlag('world', 'feature.gt.turn', currentTurn);
+let oldDamageRoll = this.damageRoll;
+let newDamageRoll = '';
+for (let i = 0; oldDamageRoll.terms.length > i; i++) {
+	let flavor = oldDamageRoll.terms[i].flavor;
+	let isDeterministic = oldDamageRoll.terms[i].isDeterministic;
+	if (isDeterministic === true) {
+		newDamageRoll += oldDamageRoll.terms[i].formula;
+	} else {
+		newDamageRoll += oldDamageRoll.terms[i].number + 'd' + oldDamageRoll.terms[i].faces + '[necrotic]';
+	}
+}
+let diceNum = 1;
+if (this.isCritical) diceNum = 2;
+let extraDice = '+ ' + diceNum + 'd' + this.damageRoll.dice[0].faces + '[necrotic]';
+let damageFormula = newDamageRoll + extraDice;
+let damageRoll = await new Roll(damageFormula).roll({async: true});
+await this.setDamageRoll(damageRoll);
