a2bfc974001713bd2850712c02ce1147433e053a
Chris <chrisk123999@gmail.com>
2023-02-08 15:57:39 -0800

    Update Chris-EatMemories.js

--- Features/Monster Features/Elder Oblex/Eat Memories/Chris-EatMemories.js
+++ Features/Monster Features/Elder Oblex/Eat Memories/Chris-EatMemories.js
@@ -33,80 +33,72 @@ let diceSize = '1d4';
 let targetActor = this.targets.first().actor;
 let effect = chris.findEffect(targetActor,'Eat Memories');
 if (effect) {
-	diceSize = effect.changes[0].value;
-	switch (diceSize) {
-		case '1d4':
-			diceSize = '1d6';
-			break;
-		case '1d6':
-			diceSize = '1d8';
-			break;
-		case '1d8':
-			diceSize = '1d10';
-			break;
-		case '1d10':
-			diceSize = '1d12';
-			break;
-		case '1d12':
-			diceSize = '1d20';
-			break;
-		case '1d20':
-			let condition = chris.findEffect(targetActor, 'Unconscious');
-			if (!condition) await chris.addCondition(targetActor, 'Unconscious', false, this.item.uuid);
-			return;
-	}
-}
-let roll = await new Roll(diceSize).evaluate({async: true});
-roll.toMessage({
-	rollMode: 'roll',
-	speaker: {alias: name},
-	flavor: this.item.name
-});
-if (effect) {
-	let changes = effect.changes;
-	changes[0].value = diceSize;
-	changes[1].value = changes[1].value - roll.total;
-	changes[2].value = changes[2].value - roll.total;
-	await chris.updateEffect(effect, {changes});
+    diceSize = effect.changes[0].value;
+    switch (diceSize) {
+        case '1d4':
+            diceSize = '1d6';
+            break;
+        case '1d6':
+            diceSize = '1d8';
+            break;
+        case '1d8':
+            diceSize = '1d10';
+            break;
+        case '1d10':
+            diceSize = '1d12';
+            break;
+        case '1d12':
+            diceSize = '1d20';
+            break;
+        case '1d20':
+            let condition = chris.findEffect(targetActor, 'Unconscious');
+            if (!condition) await chris.addCondition(targetActor, 'Unconscious', false, this.item.uuid);
+            return;
+    }
+    let changes = effect.changes;
+    changes[0].value = diceSize;
+    changes[1].value = '-' + diceSize;
+    changes[2].value = '-' + diceSize;
+    await chris.updateEffect(effect, {changes});
 } else {
-	let effectData = {
-		'label': this.item.name,
-		'icon': this.item.img,
-		'origin': this.item.uuid,
-		'duration': {
-			'seconds': 604800
-		},
-		'changes': [
-			{
-				'key': 'flags.world.feature.eatmemories',
-				'mode': 5,
-				'value': diceSize,
-				'priority': 20
-			},
-			{
-				'key': 'system.bonuses.All-Attacks',
-				'mode': 2,
-				'value': -roll.total,
-				'priority': 20
-			},
-			{
-				'key': 'system.bonuses.abilities.check',
-				'mode': 2,
-				'value': -roll.total,
-				'priority': 20
-			}
-		],
-		'flags': {
-			'dae': {
-				'transfer': false,
-				'specialDuration': [
-					'longRest',
-					'shortRest'
-				],
-				'stackable': 'multi',
-				'macroRepeat': 'none'
-			}
-		}
-	};
-	await chris.createEffect(targetActor, effectData);
+    let effectData = {
+        'label': this.item.name,
+        'icon': this.item.img,
+        'origin': this.item.uuid,
+        'duration': {
+            'seconds': 604800
+        },
+        'changes': [
+            {
+                'key': 'flags.world.feature.eatmemories',
+                'mode': 5,
+                'value': diceSize,
+                'priority': 20
+            },
+            {
+                'key': 'system.bonuses.All-Attacks',
+                'mode': 2,
+                'value': '-' + diceSize,
+                'priority': 20
+            },
+            {
+                'key': 'system.bonuses.abilities.check',
+                'mode': 2,
+                'value': '-' + diceSize,
+                'priority': 20
+            }
+        ],
+        'flags': {
+            'dae': {
+                'transfer': false,
+                'specialDuration': [
+                    'longRest',
+                    'shortRest'
+                ],
+                'stackable': 'multi',
+                'macroRepeat': 'none'
+            }
+        }
+    };
+    await chris.createEffect(targetActor, effectData);
 }
