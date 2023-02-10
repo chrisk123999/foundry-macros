a81838bba34cd16b429e31fac71039ccc8d155d5
Chris <chrisk123999@gmail.com>
2023-02-10 15:21:37 -0800

    Update Chris-ChillTouch.js

--- Spells/Chill Touch/Chris-ChillTouch.js
+++ Spells/Chill Touch/Chris-ChillTouch.js
@@ -1,8 +1,16 @@
+let chris = {
+	'findEffect': function _findEffect(actor, name) {
+		return actor.effects.find(eff => eff.label === name);
+	},
+	'raceOrType': function _raceOrType(actor) {
+		return actor.type === "npc" ? actor.system.details?.type?.value : actor.system.details?.race;
+	}
+};
 if (this.targets.size != 1) return;
 if (this.disadvantage) return;
-let type = this.actor.type === "npc" ? this.actor.system.details?.type?.value : this.actor.system.details?.race;
+let type = chris.raceOrType(this.actor);
 if (type != 'undead') return;
-let effect = this.actor.effects.find(eff => eff.label === 'Chill Touch');
+let effect = chris.findEffect(this.actor, 'Chill Touch');
 if (!effect) return;
 let sourceActor = await fromUuid(effect.origin);
 let sourceActorId = sourceActor.actor.id;
