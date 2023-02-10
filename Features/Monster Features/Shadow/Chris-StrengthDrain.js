72dccffeaa21a2eb2fadbd9bad50c0d516b9cd35
Chris <chrisk123999@gmail.com>
2023-02-09 23:33:00 -0800

    Update Chris-StrengthDrain.js

--- Features/Monster Features/Shadow/Chris-StrengthDrain.js
+++ Features/Monster Features/Shadow/Chris-StrengthDrain.js
@@ -1,60 +1,59 @@
 let chris = {
-    'findEffect': function _findEffect(actor, name) {
-        return actor.effects.find(eff => eff.label === name);
-    },
-    'createEffect': async function _createEffect(actor, effectData) {
-        if (game.user.isGM) {
-            await actor.createEmbeddedDocuments('ActiveEffect', [effectData]);
-        } else {
-            await MidiQOL.socket().executeAsGM('createEffects', {'actorUuid': actor.uuid, 'effects': [effectData]});
-        }
-    }
-    'updateEffect': async function _updateEffect(effect, updates) {
-        if (game.user.isGM) {
-            await effect.update(updates);
-        } else {
-            updates._id = effect.id;
-            await MidiQOL.socket().executeAsGM('updateEffects', {'actorUuid': effect.parent.uuid, 'updates': [updates]});
-        }
-    },
-    'addCondition': async function _addCondition(actor, name, overlay, origin) {
-        await game.dfreds.effectInterface.addEffect(
-            {
-                'effectName': name,
-                'uuid': actor.uuid,
-                'origin': origin,
-                'overlay': overlay
-            }
-        );
-    },
-    'removeCondition': async function _removeCondition(actor, name) {
-        await game.dfreds.effectInterface.removeEffect(
-            {
-                'effectName': name,
-                'uuid': actor.uuid
-            }
-        );
-    }
+	'findEffect': function _findEffect(actor, name) {
+		return actor.effects.find(eff => eff.label === name);
+	},
+	'createEffect': async function _createEffect(actor, effectData) {
+		if (game.user.isGM) {
+			await actor.createEmbeddedDocuments('ActiveEffect', [effectData]);
+		} else {
+			await MidiQOL.socket().executeAsGM('createEffects', {'actorUuid': actor.uuid, 'effects': [effectData]});
+		}
+	},
+	'updateEffect': async function _updateEffect(effect, updates) {
+		if (game.user.isGM) {
+			await effect.update(updates);
+		} else {
+			updates._id = effect.id;
+			await MidiQOL.socket().executeAsGM('updateEffects', {'actorUuid': effect.parent.uuid, 'updates': [updates]});
+		}
+	},
+	'addCondition': async function _addCondition(actor, name, overlay, origin) {
+		await game.dfreds.effectInterface.addEffect(
+			{
+				'effectName': name,
+				'uuid': actor.uuid,
+				'origin': origin,
+				'overlay': overlay
+			}
+		);
+	},
+	'removeCondition': async function _removeCondition(actor, name) {
+		await game.dfreds.effectInterface.removeEffect(
+			{
+				'effectName': name,
+				'uuid': actor.uuid
+			}
+		);
+	}
 };
-let workflow = args[0].workflow;
-if (workflow.hitTargets.size != 1) return;
+if (this.hitTargets.size != 1) return;
 let roll = await new Roll('1d4').roll({async: true});
 roll.toMessage({
-    rollMode: 'roll',
-    speaker: {alias: name},
-    flavor: 'Strength Drain'
+	rollMode: 'roll',
+	speaker: {alias: name},
+	flavor: 'Strength Drain'
 });
-let targetToken = workflow.hitTargets.first();
+let targetToken = this.hitTargets.first();
 let targetActor = targetToken.actor;
 let targetEffect = chris.findEffect(targetActor, 'Strength Drain');
 if (!targetEffect) {
-    let effectData = {
+	let effectData = {
 		'label': 'Strength Drain',
 		'icon': 'icons/skills/wounds/injury-hand-blood-red.webp',
 		'duration': {
 			'seconds': 604800
 		},
-		'origin': workflow.item.uuid,
+		'origin': this.item.uuid,
 		'changes': [
 			{
 				'key': 'system.abilities.str.value',
@@ -74,13 +73,13 @@ if (!targetEffect) {
 	};
 	await chris.createEffect(targetActor, effectData);
 } else {
-    let changes = targetEffect.changes;
-    changes[0].value -= roll.total;
-    let updates = {changes};
-    await chris.updateEffect(targetEffect, updates);
+	let changes = targetEffect.changes;
+	changes[0].value -= roll.total;
+	let updates = {changes};
+	await chris.updateEffect(targetEffect, updates);
 }
 let targetStrength = targetActor.system.abilities.str.value;
 if (targetStrength <= 0) {
-    await chris.removeCondition(targetActor, 'Unconscious');
-    await chris.addCondition(targetActor, 'Dead', true, null);
+	await chris.removeCondition(targetActor, 'Unconscious');
+	await chris.addCondition(targetActor, 'Dead', true, null);
 }
