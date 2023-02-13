92f8597b016b1507128615fcbdafb2888d92c736
Chris <chrisk123999@gmail.com>
2023-02-12 22:41:25 -0800

    Code Cleanup

--- Features/Class Features/Artificer/Armorer/Armor Swap/Chris-ArmorerEquipSwap.js
+++ Features/Class Features/Artificer/Armorer/Armor Swap/Chris-ArmorerEquipSwap.js
@@ -1,27 +1,25 @@
-let item = args[0].item;
-let actor = args[0].actor;
 let effect;
 let doLateUpdate = false;
 async function lateUpdate () {
-    await warpgate.wait(2000);
-    let fieldUses = await actor.getFlag('world', 'feature.defensiveField');
-    if (!fieldUses) fieldUses = actor.system.attributes.prof;
-    let fieldItem = actor.items.getName('Guardian Armor: Defensive Field');
-    if (fieldItem) await fieldItem.update({
-        'system.uses.value': fieldUses,
-        'system.uses.max': actor.system.attributes.prof
-    });
+	await warpgate.wait(2000);
+	let fieldUses = await this.actor.getFlag('world', 'feature.defensiveField');
+	if (!fieldUses) fieldUses = this.actor.system.attributes.prof;
+	let fieldItem = this.actor.items.getName('Guardian Armor: Defensive Field');
+	if (fieldItem) await fieldItem.update({
+		'system.uses.value': fieldUses,
+		'system.uses.max': this.actor.system.attributes.prof
+	});
 }
-switch (item.name) {
-    case 'Arcane Armor: Guardian Model':
-        effect = actor.effects.find(eff => eff.label === 'Arcane Armor: Infiltrator Model');
-        doLateUpdate = true;
-        break;
-    case 'Arcane Armor: Infiltrator Model':
-        effect = actor.effects.find(eff => eff.label === 'Arcane Armor: Guardian Model');
-        let defensiveFieldItem = actor.items.getName('Guardian Armor: Defensive Field');
-        if (defensiveFieldItem) actor.setFlag('world', 'feature.defensiveField', defensiveFieldItem.system.uses.value);
-        break;
+switch (this.item.name) {
+	case 'Arcane Armor: Guardian Model':
+		effect = this.actor.effects.find(eff => eff.label === 'Arcane Armor: Infiltrator Model');
+		doLateUpdate = true;
+		break;
+	case 'Arcane Armor: Infiltrator Model':
+		effect = this.actor.effects.find(eff => eff.label === 'Arcane Armor: Guardian Model');
+		let defensiveFieldItem = this.actor.items.getName('Guardian Armor: Defensive Field');
+		if (defensiveFieldItem) this.actor.setFlag('world', 'feature.defensiveField', defensiveFieldItem.system.uses.value);
+		break;
 }
 if (effect) effect.delete();
 if (doLateUpdate) lateUpdate();
--- Features/Class Features/Artificer/Armorer/Armor Swap/Chris-ArmorerToggle.js
+++ Features/Class Features/Artificer/Armorer/Armor Swap/Chris-ArmorerToggle.js
@@ -1,9 +1,9 @@
 let effect = actor.effects.find(eff => eff.label === 'Arcane Armor: Infiltrator Model');
 let feature;
 if (effect) {
-    feature = args[0].actor.items.getName('Arcane Armor: Guardian Model');
+	feature = this.actor.items.getName('Arcane Armor: Guardian Model');
 } else {
-    feature = args[0].actor.items.getName('Arcane Armor: Infiltrator Model');
+	feature = this.actor.items.getName('Arcane Armor: Infiltrator Model');
 }
 if (!feature) return;
-feature.roll();
+await feature.use();
