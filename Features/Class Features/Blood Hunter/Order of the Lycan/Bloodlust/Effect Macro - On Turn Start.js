386eef6749852a9752b9a65506d67a33ce9ff7e7
Chris <chrisk123999@gmail.com>
2023-02-12 22:37:14 -0800

    Code Cleanup

--- Features/Class Features/Blood Hunter/Order of the Lycan/Bloodlust/Effect Macro - On Turn Start.js
+++ Features/Class Features/Blood Hunter/Order of the Lycan/Bloodlust/Effect Macro - On Turn Start.js
@@ -1,4 +1,4 @@
 if ((actor.system.attributes.hp.max / 2) < actor.system.attributes.hp.value) return;
 let feature = actor.items.getName('Hybrid Transformation Features: Bloodlust');
 if (!feature) return;
-await feature.roll();
+await feature.use();
