54aa869ce9ae7804707cf665e7fb7863f7dffabe
Chris <chrisk123999@gmail.com>
2023-02-14 13:16:59 -0800

    Update Chris-DeathWardWorld.js

--- Spells/Death Ward/Chris-DeathWardWorld.js
+++ Spells/Death Ward/Chris-DeathWardWorld.js
@@ -1,8 +1,8 @@
 Hooks.on('midi-qol.damageApplied', async (token, {item, workflow, ditem}) => {
-    let effect = token.actor.effects.find(eff => eff.label === 'Death Ward');
-    if (!effect) return;
-    if (ditem.newHP != 0) return;
-    ditem.newHP = 1;
-    ditem.hpDamage = Math.abs(ditem.newHP - ditem.oldHP);
+	let effect = token.actor.effects.find(eff => eff.label === 'Death Ward');
+	if (!effect) return;
+	if (ditem.newHP != 0) return;
+	ditem.newHP = 1;
+	ditem.hpDamage = Math.abs(ditem.newHP - ditem.oldHP);
 	await MidiQOL.socket().executeAsGM('removeEffects', {'actorUuid': effect.parent.uuid, 'effects': [effect.id]});
 });
