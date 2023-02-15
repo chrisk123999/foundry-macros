324d04dfa1f104a0c6e7412483c8320e3c71cd0f
Chris <chrisk123999@gmail.com>
2023-02-15 12:12:45 -0800

    Code Cleanup

--- Features/Class Features/Paladin/Chris-DivineSmite.js
+++ Features/Class Features/Paladin/Chris-DivineSmite.js
@@ -1,21 +1,20 @@
 let chris = {
-    'dialog': async function _dialog(title, options) {
-        let buttons = options.map(([label,value]) => ({label,value}));
-        let selected = await warpgate.buttonDialog(
-            {
-                buttons,
-                title,
-            },
-            'column'
-        );
-        return selected;
-    }
+	'dialog': async function _dialog(title, options) {
+		let buttons = options.map(([label,value]) => ({label,value}));
+		let selected = await warpgate.buttonDialog(
+			{
+				buttons,
+				title,
+			},
+			'column'
+		);
+		return selected;
+	}
 };
-if (args[0].hitTargets.length != 1) return;
-if (args[0].item.type != 'weapon') return;
-if (!(args[0].item.system.weaponType === 'martialM' || args[0].item.system.weaponType === 'simpleM')) return;
-let actor = args[0].actor;
-let spells = actor.system.spells;
+if (this.hitTargets.size != 1) return;
+if (this.item.type != 'weapon') return;
+if (!(this.item.system.weaponType === 'martialM' || this.item.system.weaponType === 'simpleM')) return;
+let spells = this.actor.system.spells;
 let pactSlots = spells.pact.value;
 let pactLevel = spells.pact.level;
 let spell1 = spells.spell1.value;
@@ -46,63 +45,61 @@ if (!selectedOption || selectedOption === 0) return;
 let update = {};
 let damageDiceNum;
 switch (selectedOption) {
-    case 'p':
-        update = {'system.spells.pact.value': pactSlots - 1};
-        damageDiceNum = pactLevel + 1;
-        break;
-    case 1:
-        update = {'system.spells.spell1.value': spell1 - 1};
-        damageDiceNum = 2;
-        break;
-    case 2:
-        update = {'system.spells.spell2.value': spell2 - 1};
-        damageDiceNum = 3;
-        break;
-    case 3:
-        update = {'system.spells.spell3.value': spell3 - 1};
-        damageDiceNum = 4;
-        break;
-    case 4:
-        update = {'system.spells.spell4.value': spell4 - 1};
-        damageDiceNum = 5;
-        break;
-    case 5:
-        update = {'system.spells.spell5.value': spell5 - 1};
-        damageDiceNum = 5;
-        break;
-    case 6:
-        update = {'system.spells.spell6.value': spell6 - 1};
-        damageDiceNum = 5;
-        break;
-    case 7:
-        update = {'system.spells.spell7.value': spell7 - 1};
-        damageDiceNum = 5;
-        break;
-    case 8:
-        update = {'system.spells.spell8.value': spell8 - 1};
-        damageDiceNum = 5;
-        break;
-    case 9:
-        update = {'system.spells.spell9.value': spell9 - 1};
-        damageDiceNum = 5;
-        break;
+	case 'p':
+		update = {'system.spells.pact.value': pactSlots - 1};
+		damageDiceNum = pactLevel + 1;
+		break;
+	case 1:
+		update = {'system.spells.spell1.value': spell1 - 1};
+		damageDiceNum = 2;
+		break;
+	case 2:
+		update = {'system.spells.spell2.value': spell2 - 1};
+		damageDiceNum = 3;
+		break;
+	case 3:
+		update = {'system.spells.spell3.value': spell3 - 1};
+		damageDiceNum = 4;
+		break;
+	case 4:
+		update = {'system.spells.spell4.value': spell4 - 1};
+		damageDiceNum = 5;
+		break;
+	case 5:
+		update = {'system.spells.spell5.value': spell5 - 1};
+		damageDiceNum = 5;
+		break;
+	case 6:
+		update = {'system.spells.spell6.value': spell6 - 1};
+		damageDiceNum = 5;
+		break;
+	case 7:
+		update = {'system.spells.spell7.value': spell7 - 1};
+		damageDiceNum = 5;
+		break;
+	case 8:
+		update = {'system.spells.spell8.value': spell8 - 1};
+		damageDiceNum = 5;
+		break;
+	case 9:
+		update = {'system.spells.spell9.value': spell9 - 1};
+		damageDiceNum = 5;
+		break;
 }
-await actor.update(update);
-let targetToken = args[0].targets[0];
+await this.actor.update(update);
+let targetToken = this.targets.first();
 let targetActor = targetToken.actor;
 let type = targetActor.system.details.type;
 if (!type) {
-    type = targetActor.system.details.race.toLowerCase();
+	type = targetActor.system.details.race.toLowerCase();
 } else if (type) {
-    type = targetActor.system.details.type.value;
+	type = targetActor.system.details.type.value;
 }
 if (type === 'undead' || type === 'fiend') damageDiceNum += 1;
-if (args[0].isCritical === true) damageDiceNum = damageDiceNum * 2;
+if (this.isCritical) damageDiceNum = damageDiceNum * 2;
 let damageDice = damageDiceNum + 'd8[radiant]';
-const workflow = MidiQOL.Workflow.getWorkflow(args[0].uuid);
-let damageFormula = workflow.damageRoll._formula + ' + ' + damageDice;
-workflow.damageRoll = await new Roll(damageFormula).roll({async: true});
-workflow.damageTotal = workflow.damageRoll.total;
-workflow.damageRollHTML = await workflow.damageRoll.render();
+let damageFormula = this.damageRoll._formula + ' + ' + damageDice;
+let damageRoll = await new Roll(damageFormula).roll({async: true});
+await this.setDamageRoll(damageRoll);
 let feature = await actor.items.getName('Divine Smite');
-await feature.roll();
+await feature.use();
